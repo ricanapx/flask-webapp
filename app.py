58ac57337b87d25c5d9935e798fe7f1eb2d922d4
ricanapx <ricana.p@hotmail.com>
2026-05-11 16:18:39 +0100

    Added a def app function, added a static file that contains .jpg for images

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from threading import Timer
 from flask import Flask, render_template, request, redirect, url_for 
 
 app = Flask(__name__) 
+
 users = []  # A list to store user data (for demonstration purposes)
 
 # Open the default web browser and navigate to the specified URL (http://localhost:5000/)
@@ -35,10 +36,13 @@ def data():
 def contact():
     return render_template('contact.html')  # Render the contact.html template
 
-# Running the app with a custom host and port, enabling debug mode, and disabling the reloader to prevent the server from restarting automatically when code changes are detected. This is useful for development purposes.
-if __name__ == '__main__': 
-    Timer(1, open_browser).start() # Start a timer that will call the open_browser function after 1 second, which will open the web browser to the specified URL.
-    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False) 
+# Function to run the Flask application.
+def app_run():
+     Timer(1, open_browser).start() # Start a timer that will call the open_browser function after 1 second, which will open the web browser to the specified URL.
+     app.run(host='0.0.0', port=5000, debug=True, use_reloader=False)
+
+# Running the app with a custom host and port, enabling debug mode, and disabling the reloader to prevent the server from restarting automatically when code changes are detected. This is useful for development purposes
+app_run() 
 
 
 
